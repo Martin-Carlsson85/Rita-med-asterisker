37b7e85cc244e9c2b28f5ed8e21c96eb25969f9c
mc22dg <mc22dg@student.lnu.se>
2014-09-18 10:20:27 +0200

    Ändring

---
+++ Rita med asterisker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Deklaration av variabl
+            int i = 1;
+
+            //for-satsen 
+            for (int x = 0; x < 26; x++)
+            {
+
+                switch (i)
+                {
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        i++;
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        i++;
+                        break;
+                    case 3:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        i = 1;
+                        break;
+                }
+                if (x % 2 != 0)
+                {
+                    Console.Write(" ");
+                }
+                for (int y = 0; y < 39; y++)
+                {
+                    Console.Write(" ");
+                    Console.Write('*');
+                }
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
